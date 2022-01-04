8e45e185e0881a407381dabd45effdd4ab71375a
unknown <sarojsaroj390@gmail.com>
2022-01-03 16:20:28 -0800

    added path to os module before join

--- indrannabio/settings.py
+++ indrannabio/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = 'static/'
 #     os.path.join(BASE_DIR, "static")
 # ]
 
-STATIC_ROOT = os.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
