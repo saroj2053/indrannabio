87398acfb095309d037db366c973956ed7a9f06f
unknown <sarojsaroj390@gmail.com>
2022-01-03 16:17:36 -0800

    static

--- indrannabio/urls.py
+++ indrannabio/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 admin.site.site_header = "IndrAnna Biometric & Security Pvt. Ltd."
 admin.site.site_title = "IndrAnna Admin Portal"
@@ -24,4 +26,4 @@ admin.site.index_title = "Welcome to IndrAnna Portal"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
