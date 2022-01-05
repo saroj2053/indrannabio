f3a392b88b0852a6b9e8791827e365ee3188e42b
unknown <sarojsaroj390@gmail.com>
2022-01-04 23:29:27 -0800

    Modified settings.py

--- indrannabio/settings.py
+++ indrannabio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%7_r6n2mc-egww=onsvv=shrcl9nhv5)s)*djnuklv*ep_i8rt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", "indrannabio.herokuapp.com"]
 
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = 'static/'
 # Added Manually
